7521608d7eed59c6666a7c94b70e16b2c2a513bc
Sayani Koley <sayanikoley8@gmail.com>
2021-06-19 11:23:27 +0530

    TC and main class to find max the maximum of 3 float values

--- MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
@@ -1,19 +1,27 @@
+/**
+ * Given 3 Floats find the maximum
+ * Ensure to test code with the Test Case
+ *
+ * @author: SAYANI KOLEY
+ * @since: 19.06.2021
+ */
+
 package com.sayani.max;
 
 public class MaxFinder {
-    public static Integer findMax(Integer int1, Integer int2, Integer int3) {
-        Integer max = int1;
-        if(int2.compareTo(max) > 0)
-            max = int2;
-        if(int3.compareTo(max) > 0)
-            max = int3;
-        System.out.printf("Max value of %s %s %s is %s. \n", int1, int2, int3, max);
+    public static Float findMax(Float float1, Float float2, Float float3) {
+        Float max = float1;
+        if(float2.compareTo(max) > 0)
+            max = float2;
+        if(float3.compareTo(max) > 0)
+            max = float3;
+        System.out.printf("Max value of %s %s %s is %s. \n", float1, float2, float3, max);
 
         return max;
     }
 
     public static void main(String[] args) {
-        Integer int1 = 1, int2 = 4, int3 = 7;
-        findMax(int1, int2, int3);
+        Float float1 = 1.34f, float2 = 4.56f, float3 = 7.98f;
+        findMax(float1, float2, float3);
     }
 }
