2bc03c606f809fcd51ce19d5e934b0fed3f591a2
Sayani Koley <sayanikoley8@gmail.com>
2021-06-19 13:26:25 +0530

    Refactor to create Generic Class to take in 3 variables of Generic Type

--- MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
@@ -1,7 +1,9 @@
 /**
- * Refactor all the 3 t One Generic Method and find the maximum
+ * Refactor to create Generic Class to take in 3 variables of Generic Type
  * Ensure the Generic Type extends Comparable
- * Make the test case work
+ * Write parameter constructor
+ * Write testMaximum method to internally call the static testMaximum method passing the 3 instance variables
+ * Define new test case to use the Generic Class
  *
  * @author: SAYANI KOLEY
  * @since: 19.06.2021
@@ -9,23 +11,77 @@
 
 package com.sayani.max;
 
-public class MaxFinder {
-    public static <T extends Comparable<T>> T findMax(T[] elements) {
-        T max = elements[0];
-        for (T i: elements) {
-            if(i.compareTo(max) > 0)
-                max = i;
-        }
+public class MaxFinder<T extends Comparable<T>> {
+    T x, y, z;
+
+    public MaxFinder() {
+    }
+
+    public  MaxFinder(T[] elements) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static Integer textMaximumInteger(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+
+        printMax(x, y, z, max);
+        return max;
+    }
+
+    public static Float textMaximumFloat(Float x, Float y, Float z) {
+        Float max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+
+        printMax(x, y, z, max);
+        return max;
+    }
+
+    public static String textMaximumString(String x, String y, String z) {
+        String max = x;
+        if(y.compareToIgnoreCase(max) > 0)
+            max = y;
+        if(z.compareToIgnoreCase(max) > 0)
+            max = z;
+
+        printMax(x, y, z, max);
+        return max;
+    }
+
+    public static <T> void printMax(T x, T y, T z, T max) {
+        System.out.printf("Max of %s %s %s is %s \n", x, y, z, max);
+    }
+
+    public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
+        T max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+
+        printMax(x, y, z, max);
         return max;
     }
 
     public static void main(String[] args) {
-        Integer[] integerValue = {10, 34, 27};
-        Float[]  floatValue = {16.75f, 2.3f, 8.76f};
-        String[] stringValue = {"apple", "banana", "orange"};
+        Integer integerValue1 = 10, integerValue2 =  34, integerValue3 = 27;
+        Float floatValue1 = 16.75f, floatValue2 = 2.3f, floatValue3 =  8.76f;
+        String stringValue1 = "apple", stringValue2 = "banana", stringValue3 = "orange";
 
-        findMax(integerValue);
-        findMax(floatValue);
-        findMax(stringValue);
+        textMaximumString(stringValue1, stringValue2, stringValue3);
+        textMaximumFloat(floatValue1, floatValue2, floatValue3);
+        textMaximumInteger(integerValue1, integerValue2, integerValue3);
+
+        findMax(integerValue1, integerValue2, integerValue3);
+        findMax(stringValue1, stringValue2, stringValue3);
+        findMax(floatValue1, floatValue2, floatValue3);
     }
 }
--- MaxFinder/src/test/java/com/sayani/max/TU_MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/TU_MaxFinder.java
@@ -7,36 +7,9 @@ public class TU_MaxFinder {
 
     //Test data has Max Number in the First position
     @Test
-    void whenString1ShouldReturnMax() {
-        String[] string1 = {"orange", "apple", "banana"};
-        String max = MaxFinder.findMax(string1);
-
-        Assertions.assertEquals("orange", max, "Validating max method");
-    }
-
-    //Test data has Max Number in the Second position
-    @Test
-    void whenString2ShouldReturnMax() {
-        String[] string1 = {"apple", "orange", "banana"};
-        String max = MaxFinder.findMax(string1);
-
-        Assertions.assertEquals("orange", max, "Validating max method");
-    }
-
-    //Test data has Max Number in the Third position
-    @Test
-    void whenString3ShouldReturnMax() {
-        String[] string1 = {"apple", "banana", "orange"};
-        String max = MaxFinder.findMax(string1);
-
-        Assertions.assertEquals("orange", max, "Validating max method");
-    }
-
-    //Test data has Max Number in the First position
-    @Test
     void whenInteger1ShouldReturnMax() {
-        Integer[] int1 = {11, 1, 6};
-        Integer max = MaxFinder.findMax(int1);
+        Integer int1 = 11, int2 = 1, int3 = 6;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
 
         Assertions.assertEquals(11, max, "Validating max method");
     }
@@ -44,8 +17,8 @@ public class TU_MaxFinder {
     //Test data has Max Number in the Second position
     @Test
     void whenInteger2ShouldReturnMax() {
-        Integer[] int1 = {1, 11, 6};
-        Integer max = MaxFinder.findMax(int1);
+        Integer int1 = 1, int2 = 11, int3 = 6;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
 
         Assertions.assertEquals(11, max, "Validating max method");
     }
@@ -53,8 +26,8 @@ public class TU_MaxFinder {
     //Test data has Max Number in the Third position
     @Test
     void whenInteger3ShouldReturnMax() {
-        Integer[] int1 = {1, 6, 11};
-        Integer max = MaxFinder.findMax(int1);
+        Integer int1 = 1, int2 = 6, int3 = 11;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
 
         Assertions.assertEquals(11, max, "Validating max method");
     }
@@ -62,8 +35,8 @@ public class TU_MaxFinder {
     //Test data has Max Number in the First position
     @Test
     void whenFloat1ShouldReturnMax() {
-        Float[] float1 = {7.98f, 4.56f, 1.34f};
-        Float max = MaxFinder.findMax(float1);
+        Float float1 = 7.98f, float2 = 4.56f, float3 = 1.34f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
 
         Assertions.assertEquals(7.98f, max, "Validating max method");
     }
@@ -71,8 +44,8 @@ public class TU_MaxFinder {
     //Test data has Max Number in the Second position
     @Test
     void whenFloat2ShouldReturnMax() {
-        Float[] float1 = {1.34f, 7.98f, 4.56f};
-        Float max = MaxFinder.findMax(float1);
+        Float float1 = 1.34f, float2 = 7.98f, float3 = 4.56f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
 
         Assertions.assertEquals(7.98f, max, "Validating max method");
     }
@@ -80,9 +53,40 @@ public class TU_MaxFinder {
     //Test data has Max Number in the Third position
     @Test
     void whenFloat3ShouldReturnMax() {
-        Float[] float1 = {1.34f, 4.56f, 7.98f};
-        Float max = MaxFinder.findMax(float1);
+        Float float1 = 1.34f, float2 = 4.56f, float3 = 7.98f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
 
         Assertions.assertEquals(7.98f, max, "Validating max method");
     }
+
+    //Test data has Max Number in the First position
+    @Test
+    void whenString1ShouldReturnMax() {
+        String string1 = "orange", string2 = "apple", string3 = "banana";
+        String max = MaxFinder.findMax(string1, string2, string3);
+
+        Assertions.assertEquals("orange", max, "Validating max method");
+    }
+
+    //Test data has Max Number in the Second position
+    @Test
+    void whenString2ShouldReturnMax() {
+        String string1 = "apple", string2 = "orange", string3 = "banana";
+        String max = MaxFinder.findMax(string1, string2, string3);
+
+        Assertions.assertEquals("orange", max, "Validating max method");
+    }
+
+    //Test data has Max Number in the Third position
+    @Test
+    void whenString3ShouldReturnMax() {
+        String string1 = "apple", string2 = "banana", string3 = "orange";
+        String max = MaxFinder.findMax(string1, string2, string3);
+
+        Assertions.assertEquals("orange", max, "Validating max method");
+    }
+
+
+
+
 }
