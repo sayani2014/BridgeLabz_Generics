b5ea9a248c3de4c23fd85cd246f6bb146d03098e
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-19 11:53:15 +0530

    Update TU_MaxFinder.java

--- MaxFinder/src/test/java/com/sayani/max/TU_MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/TU_MaxFinder.java
@@ -4,9 +4,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TU_MaxFinder {
+    
+    //Test data has Max Number in the First position
     @Test
-    void whenIntegerShouldReturnMax() {
+    void whenInteger1ShouldReturnMax() {
+        Integer int1 = 11, int2 = 1, int3 = 6;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+
+        Assertions.assertEquals(11, max, "Validating max method");
+    }
+   
+    //Test data has Max Number in the Second position
+    @Test
+    void whenInteger2ShouldReturnMax() {
         Integer int1 = 1, int2 = 11, int3 = 6;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+
+        Assertions.assertEquals(11, max, "Validating max method");
+    }
+
+    //Test data has Max Number in the Third position
+    @Test
+    void whenInteger3ShouldReturnMax() {
+        Integer int1 = 1, int2 = 6, int3 = 11;
         Integer max = MaxFinder.findMax(int1, int2, int3);
 
         Assertions.assertEquals(11, max, "Validating max method");
