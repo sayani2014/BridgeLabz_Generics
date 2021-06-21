767dfa619103a33643c9da4ed11defb5c47fd4ac
Sayani Koley <sayanikoley8@gmail.com>
2021-06-21 15:23:38 +0530

    Extended the code to accept more parameters and use the sort() to get the maximum value

--- MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
@@ -1,6 +1,6 @@
 /**
- * Extend the max method to also print the max to std out using Generic Method
- * Write printMax Generic Method which is internally called from testMaximum
+ * Extend the max method to take more then three parameters
+ * Use options and Sorting
  *
  * @author: SAYANI KOLEY
  * @since: 19.06.2021
@@ -8,77 +8,42 @@
 
 package com.sayani.max;
 
-public class MaxFinder<T extends Comparable<T>> {
-    T x, y, z;
+import java.util.Arrays;
+import java.util.Collections;
 
-    public MaxFinder() {
-    }
+public class MaxFinder<T extends Comparable<T>> {
+    T[] elements;
 
     public  MaxFinder(T[] elements) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.elements = elements;
     }
 
-    public static Integer textMaximumInteger(Integer x, Integer y, Integer z) {
-        Integer max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-
-        printMax(x, y, z, max);
-        return max;
+    public T maximum() {
+        return MaxFinder.findMax(elements);
     }
 
-    public static Float textMaximumFloat(Float x, Float y, Float z) {
-        Float max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-
-        printMax(x, y, z, max);
-        return max;
+    public static <T> void printMax(T[] elements, T max) {
+        for (T element : elements) {
+            System.out.print(element);
+        }
+        System.out.printf("Max is %s \n", max);
     }
 
-    public static String textMaximumString(String x, String y, String z) {
-        String max = x;
-        if(y.compareToIgnoreCase(max) > 0)
-            max = y;
-        if(z.compareToIgnoreCase(max) > 0)
-            max = z;
+    public static <T extends Comparable<T>> T findMax(T[] elements) {
+        Arrays.sort(elements);
+        int length = elements.length;
+        T max = elements[length - 1];
 
-        printMax(x, y, z, max);
-        return max;
-    }
-
-    public static <T> void printMax(T x, T y, T z, T max) {
-        System.out.printf("Max of %s %s %s is %s \n", x, y, z, max);
-    }
-
-    public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
-        T max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-
-        printMax(x, y, z, max);
         return max;
     }
 
     public static void main(String[] args) {
-        Integer integerValue1 = 10, integerValue2 =  34, integerValue3 = 27;
-        Float floatValue1 = 16.75f, floatValue2 = 2.3f, floatValue3 =  8.76f;
-        String stringValue1 = "apple", stringValue2 = "banana", stringValue3 = "orange";
+        Integer[] integerValue = {10, 34, 27};
+        Float[] floatValue = {16.75f, 2.3f, 8.76f, 45.76f};
+        String[] stringValue = {"apple", "banana", "orange", "peach", "pomegranate"};
 
-        textMaximumString(stringValue1, stringValue2, stringValue3);
-        textMaximumFloat(floatValue1, floatValue2, floatValue3);
-        textMaximumInteger(integerValue1, integerValue2, integerValue3);
-
-        findMax(integerValue1, integerValue2, integerValue3);
-        findMax(stringValue1, stringValue2, stringValue3);
-        findMax(floatValue1, floatValue2, floatValue3);
+        new MaxFinder(integerValue).maximum();
+        new MaxFinder(floatValue).maximum();
+        new MaxFinder(stringValue).maximum();
     }
 }
