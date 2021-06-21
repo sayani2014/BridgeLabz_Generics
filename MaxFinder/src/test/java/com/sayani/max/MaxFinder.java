badf0df151b9ba17bd325b2dbe9e49d33601d326
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-21 15:31:40 +0530

    Updated the code with a print()

--- MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
+++ MaxFinder/src/test/java/com/sayani/max/MaxFinder.java
@@ -8,42 +8,42 @@
 
 package com.sayani.max;
 
-public class MaxFinder<T extends Comparable<T>> {
-    T x, y, z;
+import java.util.Arrays;
+import java.util.Collections;
 
-    public  MaxFinder(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+public class MaxFinder<T extends Comparable<T>> {
+    T[] elements;
+
+    public  MaxFinder(T[] elements) {
+        this.elements = elements;
     }
 
     public T maximum() {
-        return MaxFinder.findMax(x, y, z);
+        return MaxFinder.findMax(elements);
     }
 
-    public static <T> void printMax(T x, T y, T z, T max) {
-        System.out.printf("Max of %s %s %s is %s \n", x, y, z, max);
+    public static <T> void printMax(T[] elements, T max) {
+        for (T element : elements) {
+            System.out.print(element);
+        }
+        System.out.printf("Max is %s \n", max);
     }
 
-    public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
-        T max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
+    public static <T extends Comparable<T>> T findMax(T[] elements) {
+        Arrays.sort(elements);
+        int length = elements.length;
+        T max = elements[length - 1];
 
-        printMax(x, y, z, max);
         return max;
     }
 
     public static void main(String[] args) {
-        Integer integerValue1 = 10, integerValue2 =  34, integerValue3 = 27;
-        Float floatValue1 = 16.75f, floatValue2 = 2.3f, floatValue3 =  8.76f;
-        String stringValue1 = "apple", stringValue2 = "banana", stringValue3 = "orange";
+        Integer[] integerValue = {10, 34, 27};
+        Float[] floatValue = {16.75f, 2.3f, 8.76f, 45.76f};
+        String[] stringValue = {"apple", "banana", "orange", "peach", "pomegranate"};
 
-        new MaxFinder(integerValue1, integerValue2, integerValue3).maximum();
-        new MaxFinder(stringValue1, stringValue2, stringValue3).maximum();
-        new MaxFinder(floatValue1, floatValue2, floatValue3).maximum();
+        new MaxFinder(integerValue).maximum();
+        new MaxFinder(floatValue).maximum();
+        new MaxFinder(stringValue).maximum();
     }
 }
-
